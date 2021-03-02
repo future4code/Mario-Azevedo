dfefb89729b2d2334689421216df443b8d46be4a
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-02 12:49:28 -0300

    Etapa 3 ok

--- semana4/planner/aula15-planner/planner/index.js
+++ semana4/planner/aula15-planner/planner/index.js
@@ -1,6 +1,9 @@
 function criarTarefa(){
     let tarefa = document.getElementById('tarefa')
+    let diaDaSemana = document.getElementById('dias-semana')
+    
     console.log(tarefa.value)
+    console.log(diaDaSemana.value)
     console.log('Funcionando')
 
 }
