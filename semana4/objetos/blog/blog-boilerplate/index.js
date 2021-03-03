62e6a91ba11a6675e7077f2adff1b4e59d6634e8
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-03 14:36:26 -0300

    Etapa 3 ok

--- semana4/objetos/blog/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -1,4 +1,14 @@
 
-const titulo = document.getElementById('titulo-post')
-const autor = document.getElementById('autor-post')
-const conteudo = document.getElementById('conteudo-post')
+
+const criarPost = () =>{
+    const titulo = document.getElementById('titulo-post')
+    const autor = document.getElementById('autor-post')
+    const conteudo = document.getElementById('conteudo-post')
+
+    let informacao = [titulo.value,autor.value,conteudo.value]
+    titulo.value = ''
+    autor.value = ''
+    conteudo.value = ''
+
+    console.log(informacao)
+}
