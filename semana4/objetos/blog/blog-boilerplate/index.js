1f4945bc378990ee0890fb8d71642f17151995d8
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-03 15:09:02 -0300

    Desafio 1 ok

--- semana4/objetos/blog/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -5,6 +5,7 @@ const criarPost = () =>{
     const autor = document.getElementById('autor-post')
     const conteudo = document.getElementById('conteudo-post')
     const container = document.getElementById('container-de-posts')
+    const imagem = document.getElementById('imagem')
 
     let informacao = [titulo.value,autor.value,conteudo.value]
     titulo.value = ''
@@ -12,6 +13,7 @@ const criarPost = () =>{
     conteudo.value = ''
 
     container.innerHTML += `<p> ${informacao}</p>`
+    container.innerHTML += `<img src=${imagem.value}>`
 
     console.log(informacao)
 }
