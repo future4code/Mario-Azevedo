41328968ef44538ba71425151e2379716514bdb0
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-03 16:54:03 -0300

    ..

--- semana4/objetos/blog/blog-boilerplate/index.js
+++ semana4/objetos/blog/blog-boilerplate/index.js
@@ -1,5 +1,5 @@
 
-
+/* 
 const criarPost = () =>{
     const titulo = document.getElementById('titulo-post')
     const autor = document.getElementById('autor-post')
@@ -16,4 +16,24 @@ const criarPost = () =>{
     container.innerHTML += `<img src=${imagem.value}>`
 
     console.log(informacao)
-}
+} */
+
+//Desafio
+
+const criarPost = () =>{
+    const titulo = document.getElementById('titulo-post')
+    const autor = document.getElementById('autor-post')
+    const conteudo = document.getElementById('conteudo-post')
+    const container = document.getElementById('container-de-posts')
+    const imagem = document.getElementById('imagem')
+
+    let informacao = [titulo.value,autor.value,conteudo.value]
+    titulo.value = ''
+    autor.value = ''
+    conteudo.value = ''
+
+    container.innerHTML += `<p> ${informacao}</p>`
+    container.innerHTML += `<img src=${imagem.value}>`
+
+    
+} 
