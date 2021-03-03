dfcbe278db1360b3d897ba3bf4586b6c83e5f48d
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-03 14:09:00 -0300

    ..

--- semana4/planner/aula15-planner/planner/index.js
+++ semana4/planner/aula15-planner/planner/index.js
@@ -8,11 +8,13 @@ function criarTarefa(){
     }
 
     document.getElementById(diasSemana.value).innerHTML += `<p>${tarefa.value}`
-    tarefa.value = ""
-    
-   
-       
+    tarefa.value = ""  
     
 }
 
+function riscado(){
+ const diasSemana = document.getElementById('dias-semana')
+ const semana = document.getElementById(diasSemana.value)
+ document.getElementsByClassName(semana).style.color ="#ffffff";
 
+}
