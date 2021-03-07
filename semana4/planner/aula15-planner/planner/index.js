d40b043fe271392d23fb32a208efab555678246e
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-07 14:42:33 -0300

    Etapa 4 ok

--- semana4/planner/aula15-planner/planner/index.js
+++ semana4/planner/aula15-planner/planner/index.js
@@ -1,6 +1,9 @@
 //Iniciando Revisão Planner
 
 let criarTarefa = () =>{
-    let tarefas = document.getElementById('tarefa')
-    let diaSemana = document.getElementById('dias-semana')
+    let tarefas = document.getElementById('tarefa').value
+    let diaSemana = document.getElementById('dias-semana').value
+
+    console.log(tarefas)
+    console.log (diaSemana)
 }
