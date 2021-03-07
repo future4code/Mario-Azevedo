fb478bcb2a0a9c9bf210e31c58466e4c1e25366a
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-07 16:22:15 -0300

    Desafio 1 ok

--- semana4/planner/aula15-planner/planner/index.js
+++ semana4/planner/aula15-planner/planner/index.js
@@ -2,9 +2,15 @@
 
  let criarTarefa = () =>{
     let tarefas = document.getElementById('tarefa')
-    let diaSemana = document.getElementById('dias-semana')    
+    let diaSemana = document.getElementById('dias-semana')  
+    if(tarefa.value === "") {
+        alert("É necessário preencher uma tarefa!!")
+        return
+    }  
     
     document.getElementById(diaSemana.value).innerHTML += "<p>- " + tarefa.value + "</p>" 
     tarefas.value = ""
+
+   
    
  } 
