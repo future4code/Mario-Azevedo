46cc330d21513de48d497d3facea0a73241f65cc
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-07 10:13:08 -0300

    Exercício 4 ok

--- semana4/funcoes/js-vanilla-template/src/index.js
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -23,3 +23,18 @@
 //e armazena na varial arrayFinal.
 //O melhor nome seria paresQuadrados
 
+//Exercício 4
+
+//(a)
+let exercicio4a = () =>{
+    console.log('Exercício 4 (a): Eu sou Caio, Tenho 23 anos, moro em São Paulo e sou estudante')
+}
+exercicio4a()
+
+//(b)
+let exercicio4b = (nome, idade, cidade, estudante) => {
+    console.log(`Exercício 4 (b): Eu sou ${nome}, tenho ${idade} anos, moro em ${cidade}
+   e ${estudante} estudante. `)
+}
+exercicio4b('Caio', 23, 'São Paulo', 'sou')
+
