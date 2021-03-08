c39cc43515eadd39841845e56e4ccc053b7273a7
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-08 13:33:40 -0300

    Etapa 12 e 13 ok

--- semana5/aula18/labetube/src/App.js
+++ semana5/aula18/labetube/src/App.js
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const titulo = "Título do vídeo"
+
   return (
    <div>
      <div className="tela-inteira">
@@ -29,31 +32,31 @@ function App() {
                 </div>
                 <div className="box-pagina-principal media2" onclick="reproduzVideo()">
                     <img src="https://picsum.photos/400/400?a=2 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media3" onclick="reproduzVideo()">
                     <img src="https://picsum.photos/400/400?a=3 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media4" onclick="reproduzVideo()">
                     <img src="https://picsum.photos/400/400?a=4 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media5" onclick="reproduzVideo()">
                     <img src="https://picsum.photos/400/400?a=5 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media6" onclick="reproduzVideo()">
                     <img src="https://picsum.photos/400/400?a=6 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media7" onclick="reproduzVideo()">
                     <img src="https://picsum.photos/400/400?a=7 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    <h4>{titulo}</h4>
                 </div>
                 <div className="box-pagina-principal media8" onclick="reproduzVideo()">
                     <img src="https://picsum.photos/400/400?a=8 " alt="" />
-                    <h4>Título do vídeo</h4>
+                    <h4>{titulo}</h4>
                 </div>
             </section>
         </main>
