0cb8a67588476e5bc78a17a7211f7c1a953b713b
MarioCaiafaFilho <caiafamariofilho@gmail.com>
2021-03-10 19:06:09 -0300

    Exercicio ok

--- semana5/aula20/insta4/src/App.js
+++ semana5/aula20/insta4/src/App.js
@@ -10,9 +10,21 @@ class App extends React.Component {
           nomeUsuario={'paulinha'}
           fotoUsuario={'https://picsum.photos/50/50'}
           fotoPost={'https://picsum.photos/200/150'}
+        />     
+
+        <Post
+        nomeUsuario={'Fofão'}
+        fotoUsuario={'http://f.i.uol.com.br/fotografia/2015/11/06/564588-400x600-1.jpeg'}
+        fotoPost={'https://pipocamoderna.com.br/wp-content/uploads/2016/10/fofao.jpg'}
+        />       
+
+        <Post
+        nomeUsuario={'Bozo'}
+        fotoUsuario={'http://g1.globo.com/Noticias/PopArte/foto/0,,15014526,00.jpg'}
+        fotoPost={'https://static.poder360.com.br/2019/08/bozo-palhaco-868x644.jpg'}
         />
-      </div>
-    );
+        </div>
+            );
   }
 }
 
--- semana5/aula20/insta4/src/components/SecaoComentario/SecaoComentario.js
+++ semana5/aula20/insta4/src/components/SecaoComentario/SecaoComentario.js
@@ -1,24 +1,29 @@
-import React, {Component} from 'react'
+import React from "react";
 import './SecaoComentario.css'
 
-export class SecaoComentario extends Component {
+export class SecaoComentario extends React.Component {
 	state = {
+		comentario: ""
 
 	}
 
-	onChangeComentario() {
+	onChangeComentario = (event) => {
+		this.setState({comentario : event.target.value})
+		console.log(this.state.comentario)
 
 	}
 
 	render() {
-		return <div className={'comment-container'}>
+		return( 
+		<div className= "comment-container">
 			<input
-				className={'input-comentario'}
-				placeholder={'Comentário'}
-				value={''}
+				className= "input-comentario"
+				placeholder= "Comentário"
+				value={this.state.comentario}
 				onChange={this.onChangeComentario}
 			/>
 			<button onClick={this.props.aoEnviar}>Enviar</button>
 		</div>
+		)
 	}
 }
